9f950b0762845c9b17f9d5582de23d8dab2b5a03
jhcamara <jhcamara@gmail.com>
2023-02-01 20:10:23 +0000

    home page redesign

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,10 @@ import { Context } from "../store/appContext";
 import homelogo from "../../img/homelogo.png";
 import "../../styles/home.css";
 import { Navigate, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import { Rainbow } from "../component/Rainbow";
+import YoutubeEmbed from "../component/YoutubeVideo";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -28,60 +32,108 @@ export const Home = () => {
   };
 
   return (
-    <div className="mt-5 container d-flex">
-      <div className="col col-6">
-        <img className="homelogo mb-5" src={homelogo}></img>
-      </div>
-      <div className="col col-6 d-flex align-items-center">
-        <div className="form ml-5 d-flex flex-column ">
-          <h3 className="mb-5">Login to your account</h3>
-          <label for="email">E-mail</label>
+    <div className=" margintop">
+      <div className="mt-5 container d-flex">
+        <div className="col col-6">
+          <img className="homelogo mb-5" src={homelogo}></img>
+        </div>
+        <div className="col col-6 d-flex flex-column align-items-start justify-content-center">
+          <h1 className="herosection">A scan away from home</h1>
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your registered e-mail"
-            className="inputfield"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-
-          <label type="password" className="password">
-            Password
-          </label>
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            className="inputfield"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-          <p>
-            <a className="boldlink" href="register-user">
-              Sign up
-            </a>{" "}
-            if you don't have an account
-          </p>
+          {/* BUTTONS */}
           <div className="d-flex align-items-center justify-content-between">
             <a
-              className="purplebutton mt-1 mb-5 w-25 text-center mr-5"
+              className="purplebutton mt-1 mb-5  w-50 text-center mr-5"
               onClick={handleClick}
             >
               Login
             </a>
             <a
-              className="ml-3 mt-1 mb-5 w-30 text-center signup"
+              className="ml-3 mt-1 mb-5 marginleft w-75 text-center signup"
               href="register-user"
             >
               Sign up
             </a>
           </div>
+        </div>
+      </div>
+      <Rainbow />
 
-          <div>
-            {activate ? <p className="error">Error, please try again</p> : null}
+      <div className="video align-self-center justify-self-center">
+      <YoutubeEmbed embedId="JgxyNIkZmLY" />
+    </div>
+
+
+      {/* FOOTER */}
+
+      <div className="footer">
+        <div className="container d-flex">
+          <div className="col col-6">
+          <h2>Runaway Pets</h2>
+          <h6>Link</h6>
+          <h6>Link</h6>
+          <h6>Link</h6>
+          <h6>Link</h6>
+          <h6>Link</h6>
           </div>
+          
+          
+          {/* LOGIN */}
+          <div className="container col col-6">
+            <div className="form ml-5 d-flex flex-column col col-6">
+              <h3 className="mb-5">Login to your account</h3>
+              <label for="email">E-mail</label>
+
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your registered e-mail"
+                className="inputfield-purple"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+
+              <label type="password" className="password">
+                Password
+              </label>
+
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                className="inputfield-purple"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+              <p>
+                <a className="boldlink" href="register-user">
+                  Sign up
+                </a>{" "}
+                if you don't have an account
+              </p>
+              <div className="d-flex align-items-center justify-content-start">
+                <a
+                  className="purplebutton mt-1 mb-5 w-25 text-center"
+                  onClick={handleClick}
+                >
+                  Login
+                </a>
+                <a
+                  className="marginleft mt-1 mb-5 w-25 text-center signup"
+                  href="register-user"
+                >
+                  Sign up
+                </a>
+              </div>
+
+              <div>
+                {activate ? (
+                  <p className="error">Error, please try again</p>
+                ) : null}
+              </div>
+            </div>
+          </div>
+          <FontAwesomeIcon className="icon" icon="fa-brands fa-instagram" />
         </div>
       </div>
     </div>
